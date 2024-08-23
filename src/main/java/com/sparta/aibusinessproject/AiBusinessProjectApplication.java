94e504d1ca0fcd8d3038cbd9dab79f85a4d2aca4
jiyeonahn <ajy7424@naver.com>
2024-08-23 10:34:26 +0900

    Initial commit

---
+++ src/main/java/com/sparta/aibusinessproject/AiBusinessProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.aibusinessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBusinessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiBusinessProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/aibusinessproject/AiBusinessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.aibusinessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBusinessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
