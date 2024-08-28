14db8b613fcce042900b875d10a37679ab48f8a1
jiyeonahn <ajy7424@naver.com>
2024-08-29 01:43:40 +0900

    [fix] errorcode 중복 제거

--- src/main/java/com/sparta/aibusinessproject/exception/ErrorCode.java
+++ src/main/java/com/sparta/aibusinessproject/exception/ErrorCode.java
@@ -12,7 +12,6 @@ public enum ErrorCode {
     DUPLICATED_PHONE(HttpStatus.CONFLICT, "휴대폰 번호가 중복됩니다."),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email이 중복됩니다."),
     DUPLICATED_USERID(HttpStatus.CONFLICT, "USER ID가 중복됩니다."),
-    DUPLICATED_USERID(HttpStatus.CONFLICT, "USER ID가 중복됩니다."),
     INVALID(HttpStatus.BAD_REQUEST, "다시 시도해 주세요."),
 
     INVALID_ORDER(HttpStatus.NOT_FOUND, "유효하지 않은 주문입니다."),
@@ -24,13 +23,10 @@ public enum ErrorCode {
     INVALID_EMAIL(HttpStatus.BAD_REQUEST, "이메일을 형식에 맞게 입력해 주세요."),
     INVALID_PHONE(HttpStatus.BAD_REQUEST, "휴대폰 번호를 형식에 맞게 입력해 주세요."),
 
-    INVALID_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 토큰입니다."),
-
     PAYMENT_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 승인된 결제입니다."),
     INVALID_PAYMENT(HttpStatus.NOT_FOUND, "유효하지 않은 결제입니다."),
     NOT_YOUR_PAYMENT(HttpStatus.BAD_REQUEST, "회원님의 결제내역이 아닙니다."),
-    ORDER_AND_PAYMENT_MISMATCH(HttpStatus.BAD_REQUEST, "결제의 주문 ID가 요청된 주문 ID와 일치하지 않습니다.");
-
+    ORDER_AND_PAYMENT_MISMATCH(HttpStatus.BAD_REQUEST, "결제의 주문 ID가 요청된 주문 ID와 일치하지 않습니다."),
 
 
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
