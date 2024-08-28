cb459f3bd4d48542578a2d4b525481b5c6ff5948
Bae Ji Won <qowhxk@naver.com>
2024-08-29 01:32:54 +0900

    [feat] ErrorCode Add

--- src/main/java/com/sparta/aibusinessproject/exception/ErrorCode.java
+++ src/main/java/com/sparta/aibusinessproject/exception/ErrorCode.java
@@ -9,7 +9,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email이 중복됩니다."),
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, "userName 이 중복됩니다."),
-    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "패스워드가 잘못되었습니다.");
+    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "패스워드가 잘못되었습니다."),
+
+    DUPLICATED_STORENAME(HttpStatus.CONFLICT, "가게 이름이 중복됩니다."),
+    INVALID_STORE(HttpStatus.NOT_FOUND,"가게 정보가 없습니다."),
+    INVALID_CATEGORY(HttpStatus.NOT_FOUND,"카테고리 정보가 없습니다. 다른 카테고리를 입력해 주세요"),
+    DUPLICATED_CATEGORY(HttpStatus.CONFLICT, "카테고리 이름이 중복됩니다."),
+    NOTFOUND_CATEGORY(HttpStatus.NOT_FOUND,"카테고리가 존재하지 않습니다."),
+    DEFAULT_VALUE(HttpStatus.BAD_REQUEST, "기본 카테고리 종류 이므로 변경이 불가능합니다.");
 
     private HttpStatus status;
     private String message;
