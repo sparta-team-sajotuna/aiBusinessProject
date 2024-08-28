78b847349408135215c22ac76fd5272a0b6da437
Bae Ji Won <qowhxk@naver.com>
2024-08-29 01:31:37 +0900

    [feat] Setting

--- src/main/java/com/sparta/aibusinessproject/AiBusinessProjectApplication.java
+++ src/main/java/com/sparta/aibusinessproject/AiBusinessProjectApplication.java
@@ -2,8 +2,10 @@ package com.sparta.aibusinessproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AiBusinessProjectApplication {
 
     public static void main(String[] args) {
