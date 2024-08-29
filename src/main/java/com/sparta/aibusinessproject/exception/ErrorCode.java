b3fab02db7fbe276e11c7bbc61f7d9126ebf501a
Bae Ji Won <qowhxk@naver.com>
2024-08-29 15:39:47 +0900

    [fix] 중복 코드 제거 및 주석 처리

--- src/main/java/com/sparta/aibusinessproject/exception/ErrorCode.java
+++ src/main/java/com/sparta/aibusinessproject/exception/ErrorCode.java
@@ -13,7 +13,6 @@ public enum ErrorCode {
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email이 중복됩니다."),
 
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, "userName 이 중복됩니다."),
-    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "패스워드가 잘못되었습니다."),
 
     DUPLICATED_STORENAME(HttpStatus.CONFLICT, "가게 이름이 중복됩니다."),
     INVALID_STORE(HttpStatus.NOT_FOUND,"가게 정보가 없습니다."),
@@ -21,7 +20,7 @@ public enum ErrorCode {
     DUPLICATED_CATEGORY(HttpStatus.CONFLICT, "카테고리 이름이 중복됩니다."),
     NOTFOUND_CATEGORY(HttpStatus.NOT_FOUND,"카테고리가 존재하지 않습니다."),
     DEFAULT_VALUE(HttpStatus.BAD_REQUEST, "기본 카테고리 종류 이므로 변경이 불가능합니다."),
-    ALREADY_CATEGORY(HttpStatus.ALREADY_REPORTED, "이미 존재하는 카테고리 입니다");
+    ALREADY_CATEGORY(HttpStatus.ALREADY_REPORTED, "이미 존재하는 카테고리 입니다"),
 
     DUPLICATED_USERID(HttpStatus.CONFLICT, "USER ID가 중복됩니다."),
     INVALID(HttpStatus.BAD_REQUEST, "다시 시도해 주세요."),
